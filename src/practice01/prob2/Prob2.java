90b9a4acf2fe33059aa8c41ec0a195815a29fcb3
bit <bit@DESKTOP-0JMIVLH>
2019-08-06 16:13:10 +0900

    2번 완료.

--- src/practice01/prob2/Prob2.java
+++ src/practice01/prob2/Prob2.java
@@ -2,5 +2,12 @@ package practice01.prob2;
 
 public class Prob2 {
 	public static void main (String args[]) {
+		for(int i=1; i<=9; i++) {
+			for(int j=i; j<=9+i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 }
